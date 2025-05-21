a2f5e40d6491240180c4d00295fddd88229a5f55
ArturZzZZzZ <artur.zub@ideasoft.io>
2025-05-21 13:33:08 +0300

    feat: update package dependencies and enhance SolanaWalletProvider with connection handling

--- src/utils/AppContext.jsx
+++ src/utils/AppContext.jsx
@@ -1,28 +1,67 @@
-import React, { createContext, useContext, useState, useEffect, version } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  version
+} from "react";
 
 const AppContext = createContext();
 export default AppContext;
 
+const SolanaWalletProvider = ({ children }) => {
+  const network = WalletAdapterNetwork.Mainnet;
+  const endpoint = "https://api.mainnet-beta.solana.com";
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  const LABELS = {
+    "change-wallet": "Change wallet",
+    connecting: "Connecting ...",
+    "copy-address": "Copy address",
+    copied: "Copied",
+    disconnect: "Disconnect",
+    "has-wallet": "Connect",
+    "no-wallet": "Connect Solana Wallet"
+  };
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
 // basic context provider
 export const AppProvider = ({ children }) => {
-    // define state
-    const [showMainNets, setShowMainNets] = useState(true);
-    const [vaultAddress, setVaultAddress] = useState('');
-    const [vaultAssetAddress, setVaultAssetAddress] = useState('');
-    const [TargetBlockchainChainId, setTargetBlockchainChainId] = useState(0);
-    const [selectedAssetKey, setSelectedAssetKey] = useState('');
-    const [selectedAsset, setSelectedAsset] = useState(null);
-    return (
-        <AppContext.Provider value={{ showMainNets, setShowMainNets,
-                                        vaultAddress, setVaultAddress,
-                                        vaultAssetAddress, setVaultAssetAddress,
-                                        TargetBlockchainChainId, setTargetBlockchainChainId,
-                                        selectedAssetKey, setSelectedAssetKey,
-                                        selectedAsset, setSelectedAsset
-         }}>
-            {children}
-        </AppContext.Provider>
-    );
+  // define state
+  const [showMainNets, setShowMainNets] = useState(true);
+  const [vaultAddress, setVaultAddress] = useState("");
+  const [vaultAssetAddress, setVaultAssetAddress] = useState("");
+  const [TargetBlockchainChainId, setTargetBlockchainChainId] = useState(0);
+  const [selectedAssetKey, setSelectedAssetKey] = useState("");
+  const [selectedAsset, setSelectedAsset] = useState(null);
+  return (
+    <AppContext.Provider
+      value={{
+        showMainNets,
+        setShowMainNets,
+        vaultAddress,
+        setVaultAddress,
+        vaultAssetAddress,
+        setVaultAssetAddress,
+        TargetBlockchainChainId,
+        setTargetBlockchainChainId,
+        selectedAssetKey,
+        setSelectedAssetKey,
+        selectedAsset,
+        setSelectedAsset
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 // custom hook
